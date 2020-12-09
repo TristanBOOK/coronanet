5e8da4f9a84579a3ad134bb9a6e3d73145c3cfd5
Lou Rizzolo <lourizzolo@hotmail.fr>
2020-12-09 16:51:47 +0100

    get_data_function

--- coronanet/data.py
+++ coronanet/data.py
@@ -4,6 +4,18 @@ import ipdb
 
 class Coronanet:
 
+    def get_raw_data(self):
+        raw_data_path = os.getenv('CORONANET_DATA')
+        if raw_data_path:
+            data_path = f'{raw_data_path}/csv/coronanet_release_allvars.csv'
+            df = pd.read_csv(data_path, encoding='mac_roman')
+            return df
+        else:
+            print("Create a env variable with this path $HOME/code/.../coronanet/data/")
+            print('Add in .zshrc this line: export CORONANET_DATA="$HOME/code/.../coronanet/data/" ')
+            return
+
+
     def get_data(self):
         """
         This function returns a Python dict.
@@ -21,3 +33,20 @@ class Coronanet:
         # Use __file__ as absolute path anchor independant of your computer
         # Make extensive use of `import ipdb; ipdb.set_trace()` to investigate what `__file__` variable is really
         # Use os.path library to construct path independent of Unix vs. Windows specificities
+
+
+if __name__ == '__main__':
+    # For introspections purpose to quickly get this functions on ipython
+    from coronanet.data import Coronanet
+    #import coronanet.data
+    coronanet_instance = Coronanet()
+    df = coronanet_instance.get_raw_data()
+    print(df.head())
+    '''
+    if df:
+        df.head()
+    else:
+        print("problem")
+    '''
+
+
