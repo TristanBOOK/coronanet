27e506189f0baec522863ae06952154d4b9fda68
Paul Charpentier <paul.charpentier10@gmail.com>
2020-12-16 18:23:21 +0100

    saving streamlit app

--- Notebooks/app_trial.py
+++ Notebooks/app_trial.py
@@ -4,12 +4,18 @@ import numpy as np
 import pandas as pd
 import zipfile
 import datetime
+from sklearn.preprocessing import MinMaxScaler
 
 zf1 = zipfile.ZipFile('../coronanet/data/cases_new_data.csv.zip')
 cases_df = pd.read_csv(zf1.open('cases_new_data.csv'))
 lma_data=pd.lma_data=pd.read_csv('https://raw.githubusercontent.com/TristanBOOK/coronanet/master/coronanet/data/LMA_Data_10_semaines_and_Pol_fam.csv')
 zf2 = zipfile.ZipFile('../coronanet/data/policies_raw.csv.zip')
-policies_df = pd.read_csv(zf1.open('policies_raw.csv'))
+policies_df = pd.read_csv(zf2.open('policies_raw.csv'))
+
+countries_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19_Unified-Dataset/master/COVID-19_LUT.csv')
+country_dict = {}
+for i in range(countries_df.shape[0]):
+    country_dict[countries_df.ID[i]] = countries_df.NameID[i]
 
 countries=lma_data.ID.unique()
 
@@ -21,43 +27,70 @@ st.markdown("""
 option = st.selectbox('Please selct a country ID',countries)
 
 case=cases_df[cases_df['ID']==option]
-case.Date=pd.to_datetime(case.Date,format="%Y-%m-%d")
+case.Date=pd.to_datetime(case.Date)
 dte1=case.Date.iloc[case.shape[0]-1]
 dte0=case.Date.iloc[case.shape[0]-22]
-death=case.Cases.iloc[case.shape[0]-1]-case.Cases.iloc[case.shape[0]-22]
+past_21d_death=case.Cases.iloc[case.shape[0]-1]-case.Cases.iloc[case.shape[0]-22]
 
-s1=f'Unfortunately, **{death}** people died from **Covid-19** in **{option}** between **{dte0}** and **{dte1}**. Select policies to save lives'
+s1=f'Unfortunately, **{int(past_21d_death)}** people died from **Covid-19** in **{country_dict[option]}** between **{dte0.strftime("%Y-%m-%d")}** and **{dte1.strftime("%Y-%m-%d")}**.'
 st.markdown(s1)
+st.markdown("""## Select policies to save lives""")
 
-pol=['H1','G5','DFhghg']
-st.write(f'The most efficient policies to save lives are the following: {pol}. Slide to select policy intensity')
+pol=['C1','C2','C3','H1','H2']
+st.write(f'The most efficient policies to save lives are the following: {pol}. Slide to select policy intensity, and simulate the efficiency of your policy')
 
-##################################################
-##################################################
-pol='C1'
 policy=policies_df
+policy=policy.fillna(0)
 policy.Date=pd.to_datetime(policy.Date)
-policy=policy[policy['Date']>(dte1-datetime.timedelta(days=1))]
+policy=policy[policy['Date']>(pd.to_datetime(dte1)-datetime.timedelta(days=21))]
 policy=policy[policy['Country']==option]
-policy=policy[['Date','ID','PolicyType','PolicyValue']]
+policy=policy[['Date','Country','PolicyType','PolicyValue']]
 
-Slide1_curr=policy[policy['PolicyType']==pol].PolicyValue.values[1]
-##################################################
-##################################################
-#Slide1_curr=1
+Slide1_curr=policy[policy['PolicyType']==pol[0]].PolicyValue.values[0]
+Slide2_curr=policy[policy['PolicyType']==pol[1]].PolicyValue.values[0]
+Slide3_curr=policy[policy['PolicyType']==pol[2]].PolicyValue.values[0]
+Slide4_curr=policy[policy['PolicyType']==pol[3]].PolicyValue.values[0]
+Slide5_curr=policy[policy['PolicyType']==pol[4]].PolicyValue.values[0]
 
-Slide2_curr=0
-Slide3_curr=0
-Slide4_curr=0
-Slide5_curr=0
+int(policies_df[policies_df['PolicyType']==pol[0]].PolicyValue.max())
 
-i_C1 = st.slider('C1', 1, 3, Slide1_curr)
-i_H2 = st.slider('H2', -1, 4, Slide2_curr)
-i_E5 = st.slider('C1', 0, 9, 0)
-i_C3 = st.slider('C1', 0, 9, 5)
-i_E1 = st.slider('C1', 0, 9, 4)
+i1_min, i1_max=(int(policies_df[policies_df['PolicyType']==pol[0]].PolicyValue.min()),
+    int(policies_df[policies_df['PolicyType']==pol[0]].PolicyValue.max()))
+i2_min, i2_max=(int(policies_df[policies_df['PolicyType']==pol[1]].PolicyValue.min()),
+    int(policies_df[policies_df['PolicyType']==pol[1]].PolicyValue.max()))
+i3_min, i3_max=(int(policies_df[policies_df['PolicyType']==pol[2]].PolicyValue.min()),
+    int(policies_df[policies_df['PolicyType']==pol[2]].PolicyValue.max()))
+i4_min, i4_max=(int(policies_df[policies_df['PolicyType']==pol[3]].PolicyValue.min()),
+    int(policies_df[policies_df['PolicyType']==pol[3]].PolicyValue.max()))
+i5_min, i5_max=(int(policies_df[policies_df['PolicyType']==pol[4]].PolicyValue.min()),
+    int(policies_df[policies_df['PolicyType']==pol[4]].PolicyValue.max()))
 
-filt = st.radio('Number of deaths',('Cases', 'Cases_New'))
+i1 = st.slider('C1', i1_min, i1_max, int(Slide1_curr))
+i2 = st.slider('H2', i2_min, i2_max, int(Slide2_curr))
+i3 = st.slider('C2', i3_min, i3_max, int(Slide3_curr))
+i4 = st.slider('CT', i4_min, i4_max, int(Slide4_curr))
+i5 = st.slider('C3', i5_min, i5_max, int(Slide5_curr))
+
+def scaler(x,mi,ma):
+    return ((x-mi)/(ma-mi))*(ma-mi)+mi
+
+i1=scaler(i1,i1_min,i1_max)
+i2=scaler(i2,i2_min,i2_max)
+i3=scaler(i3,i3_min,i3_max)
+i4=scaler(i4,i4_min,i4_max)
+i5=scaler(i5,i5_min,i5_max)
+
+inputs=[i1,i2,i3,i4,i5,past_21d_death]
+
+############
+############
+# ADD MODEL#
+# inputs= i1,i2,i3,i4,i5,    + past_21d_death
+# return J21= number of death in 21 days predicted with model
+############
+############
+
+filt = st.radio('Display options:',('Cases', 'Cases_New'))
 
 case=case[['Date',filt]]
 
@@ -66,17 +99,11 @@ predict=list(case[filt].values)
 for i in range (21):
     predict.append(round(predict[len(predict)-1]*delta))
 
-############
-############
-# ADD MODEL#
-# J21= number of death in 21 days predicted with model
-############
-############
-
 if filt == 'Cases':
-    J21=60000 ### to be changed!!!!
+    J21=60000 ### to be changed!!!! output from model
 else:
-    J21=100 ### to be changed!!!!
+    J21=100 ### to be changed!!!! output from model
+
 
 delta21=((J21)/(case[filt].iloc[case.shape[0]-1]+1))**(1/20)
 predict21=list(case[filt].values)
